55135033691fe3d001734e4432b1c3336e06dfe3
tyeethiopia <76423328+tyeethiopia@users.noreply.github.com>
2021-03-15 12:13:30 -0400

    Apple React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Component/Header";
+import Main from "./Component/Main";
+import Footer from "./Component/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,80 @@
+import React from "react";
+import Logo from "./images/icons/logo-sm.png";
+import Search from "./images/icons/search-icon-sm.png";
+import Cart from "./images/icons/cart-sm.png";
+
+const Header = () => {
+	return (
+		<div>
+			<div className="nav-wrapper fixed-top">
+				<div className="container">
+					<nav className="navbar navbar-toggleable-sm navbar-expand-md">
+						<button
+							className="navbar-toggler navbar-toggler-right"
+							type="button"
+							data-toggle="collapse"
+							data-target=".navbar-collapse"
+						>
+							☰
+						</button>
+						<a className="navbar-brand mx-auto" href="#">
+							<img src={Logo} />
+						</a>
+
+						<div className="navbar-collapse collapse">
+							<ul className="navbar-nav nav-justified w-100 nav-fill">
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="/mac/">
+										Mac
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="#">
+										iphone
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="#">
+										ipad
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="#">
+										watch
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="#">
+										tv
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="#">
+										Music
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="#">
+										Support
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="/search/">
+										<img src={Search} />
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link js-scroll-trigger" href="/cart/">
+										<img src={Cart} />
+									</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
